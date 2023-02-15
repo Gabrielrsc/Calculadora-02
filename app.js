109c2e8f3b472280066fded1eabb69090e7a1236
Gabrielrsc <gabrielrobertson.s@gmail.com>
2023-02-15 11:54:36 -0300

    inico_class_calculadora

--- app.js
+++ app.js
@@ -9,3 +9,46 @@ function MudarTema() {
     img.setAttribute('src', './assets/dark.svg')
   }
 }
+
+const numeroBotoes = document.querySelectorAll('[data-numero]')
+const operadorBotao = document.querySelectorAll('[data-operador]')
+const igualBotao = document.querySelector('[data-igual]')
+const deleteBotao = document.querySelector('[data-delete]')
+const limparBotao = document.querySelector('[data-limpar]')
+const previuOperacaoTexto = document.querySelector('[data-previu-operacao]')
+const previuResultadoTexto = document.querySelector('[data-previu-resultado]')
+
+class Calculadora {
+  constructor(previuOperacaoTexto, previuResultadoTexto) {
+    this.previuOperacaoTexto = previuOperacaoTexto
+    this.previuResultadoTexto = previuResultadoTexto
+  }
+
+  imprimirNumero(numero) {
+    this.previuResultado = `${this.previuResultado}${numero.toString()}`
+  }
+
+  limpar() {
+    this.previuResultado = ''
+    this.preivuOperacao = ''
+    this.operacao = undefined
+  }
+
+  atualizarDisplay() {
+    this.previuOperacaoTexto.innerText = this.preivuOperacao
+    this.previuResultadoTexto.innerText = this.previuResultado
+  }
+}
+const calculadora = new Calculadora(previuOperacaoTexto, previuResultadoTexto)
+
+for (const numeroBotao of numeroBotoes) {
+  numeroBotao.addEventListener('clcik', () => {
+    calculadora.imprimirNumero(numeroBotao.innerText)
+    calculadora.atualizarDisplay()
+  })
+}
+
+limparBotao.addEventListener('click', () => {
+  calculadora.limpar()
+  calculadora.atualizarDisplay()
+})
